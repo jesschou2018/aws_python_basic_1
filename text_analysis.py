49007bad847594bb29e7751266cd8d68925a58b4
Jess Chou <jesschou2018@gmail.com>
2021-11-22 09:16:56 +0000

    Converting to classes

--- text_analysis.py
+++ text_analysis.py
@@ -1,35 +1,14 @@
+# Imports
 import argparse
-# import regex module
-import re
+from sm_text import *
 
+# Set up parser to take input variable as paremeter
 parser = argparse.ArgumentParser()
 parser.add_argument("--val")
 args = parser.parse_args()
 val = args.val
 
-def text_analysis(input_text):
-# clean input and convert to list for # and @ extraction 
-    #punctiontoreplace=[',','.','/',';','|','}','{','[',']','!','$','%','^','&','*','(',')']
-    clean_text = input_text.replace(';'," ") 
-    lower_text=clean_text.lower()
-    all_words=lower_text.split()
-# word count and text size
-    size_text=len(lower_text)
-    word_count=len(all_words)
-
-# hashtag and @locations
-    tag_locations=list(map(lambda x:x.find("#"),all_words))
-    tag_list_loc = [i for i, e in enumerate(tag_locations) if e == 0]
-    tag_values=[all_words[i] for i in tag_list_loc]
-    at_locations=list(map(lambda x:x.find("@"),all_words))
-    at_list_loc = [i for i, e in enumerate(at_locations) if e == 0]
-    at_values=[all_words[i] for i in at_list_loc]
-    return(all_words, size_text, word_count,tag_values, at_values)
-
-results=text_analysis(val)
-print(results)
-
-
-
-
-        
+# run sm_text
+text1 = sm_text(val)
+print(sm_text.extract_at_tags(text1))
+print(sm_text.extract_hashtags(text1))
